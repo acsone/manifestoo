33ca74c216f47612d5cbe8701b0fb4d01823e2a8
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-05-22 19:05:45 +0200

    Improve Odoo series detection

--- src/manifestoo/odoo_series.py
+++ src/manifestoo/odoo_series.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, Set
 
 from . import echo
 from .addons_set import AddonsSet
@@ -26,7 +26,7 @@ def detect_from_addon_version(version: str) -> Optional[OdooSeries]:
 
 
 def detect_from_addons_set(addons_set: AddonsSet) -> Optional[OdooSeries]:
-    detected: Optional[OdooSeries] = None
+    detected: Set[OdooSeries] = set()
     for addon in addons_set.values():
         addon_version = addon.manifest.version
         if not addon_version:
@@ -34,13 +34,13 @@ def detect_from_addons_set(addons_set: AddonsSet) -> Optional[OdooSeries]:
         addon_series = detect_from_addon_version(addon_version)
         if not addon_series:
             continue
-        if detected is None:
-            detected = addon_series
-        if addon_series != detected:
-            # different versions detected in the set
-            echo.notice(
-                f"Different Odoo series detected in addons set: "
-                f"{addon_series}, {detected}"
-            )
-            return None
-    return detected
+        detected.add(addon_series)
+    if len(detected) == 0:
+        echo.notice("No Odoo series detected in addons set")
+        return None
+    elif len(detected) > 1:
+        echo.notice(
+            f"Different Odoo series detected in addons set: {', '.join(detected)}"
+        )
+        return None
+    return detected.pop()
--- tests/test_odoo_series.py
+++ tests/test_odoo_series.py
@@ -1,10 +1,46 @@
-from manifestoo.odoo_series import OdooSeries, detect_from_addon_version
+from manifestoo.odoo_series import (
+    OdooSeries,
+    detect_from_addon_version,
+    detect_from_addons_set,
+)
+
+from .common import mock_addons_set
 
 
-def test_detect():
+def test_detect_from_version():
     assert detect_from_addon_version("1.0.0") is None
     assert detect_from_addon_version("1.0.0.0.0.0") is None
     assert detect_from_addon_version("8.0.1") is None
     assert detect_from_addon_version("8.0.1.0") is None
     assert detect_from_addon_version("8.0.1.0.0") == OdooSeries.v8_0
     assert detect_from_addon_version("14.0.1.0.0.1") == OdooSeries.v14_0
+
+
+def test_detect_none():
+    addons_set = mock_addons_set(
+        {
+            "a": {"version": "1.0.0"},
+            "b": {},
+        }
+    )
+    assert detect_from_addons_set(addons_set) is None
+
+
+def test_detect_one():
+    addons_set = mock_addons_set(
+        {
+            "a": {"version": "1.0.0"},
+            "b": {"version": "12.0.1.0.0"},
+        }
+    )
+    assert detect_from_addons_set(addons_set) is OdooSeries.v12_0
+
+
+def test_detect_ambiguous():
+    addons_set = mock_addons_set(
+        {
+            "a": {"version": "13.0.1.0.0"},
+            "b": {"version": "12.0.1.0.0"},
+        }
+    )
+    assert detect_from_addons_set(addons_set) is None
