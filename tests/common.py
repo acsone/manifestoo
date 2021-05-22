1ebbdf172e2908fecb527bd5eb5afefc0c9e9aca
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-05-22 19:30:56 +0200

    Tweak test common

--- tests/common.py
+++ tests/common.py
@@ -17,16 +17,17 @@ def populate_addons_dir(addons_dir: Path, addons: Dict[str, Dict[str, Any]]):
         manifest_path.write_text(repr(manifest))
 
 
-def mock_manifest(manifest_dict: Dict[str, Any]) -> Manifest:
-    manifest_path = Path("/tmp/fake-addons-dir") / "fake-addon" / "__manifest__.py"
+def mock_manifest(
+    manifest_dict: Dict[str, Any], addon_name: str = "fake-addon"
+) -> Manifest:
+    manifest_path = Path("/tmp/fake-addons-dir") / addon_name / "__manifest__.py"
     return Manifest(manifest_path, manifest_dict)
 
 
 def mock_addons_set(addons: Dict[str, Dict[str, Any]]) -> AddonsSet:
     addons_set = AddonsSet()
     for addon_name, manifest_dict in addons.items():
-        manifest_path = Path("/tmp/fake-addons-dir") / addon_name / "__manifest__.py"
-        manifest = Manifest(manifest_path, manifest_dict)
+        manifest = mock_manifest(manifest_dict, addon_name)
         addons_set[addon_name] = Addon(manifest)
     return addons_set
 
